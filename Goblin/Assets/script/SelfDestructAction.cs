8091e8e546ba803cecd25c472363b79d1899f1fb
Erik konkell <konkell.erik@gmail.com>
2019-01-29 13:14:55 +0100

    added a self destroing goblin

---
+++ Goblin/Assets/script/SelfDestructAction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Actions/SelfDestruct")]
+public class SelfDestructAction : AIAction
+{
+    public override void Act(StateController controller)
+    {
+        controller.GetComponent<vp_DamageHandler>().Damage(Mathf.Infinity);
+    }
+
+    public override void Setup(StateController controller)
+    {
+        
+    }
+}
