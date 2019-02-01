698aa690b14272ea290fb7826632043210d59f38
MrTacocake <maxdorper@gmail.com>
2019-02-01 14:21:49 +0100

    added impact grenade and new shotgun skin

---
+++ Goblin/Assets/script/ImpactGrenade.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactGrenade : MonoBehaviour
+{
+    private void Start()
+    {
+        CapsuleCollider grenadeCol;
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        GetComponent<vp_DamageHandler>().Damage(1);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<vp_DamageHandler>().Damage(1);
+    }
+
+}
