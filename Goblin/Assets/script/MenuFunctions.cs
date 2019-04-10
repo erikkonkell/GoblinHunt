33d7b57267b3d0ae31c465d1fd9387e196ed5ae6
Erik konkell <konkell.erik@gmail.com>
2019-04-10 14:03:26 +0200

    start menu and funktions

---
+++ Goblin/Assets/script/MenuFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuFunctions : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("map01");
+    }
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
